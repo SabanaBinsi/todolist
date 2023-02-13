c2efb0edfca5779680bafb9b16e7d0ceb7138a77
SabanaBinsi <binsishabana@gmail.com>
2023-02-13 12:40:49 +0530

    Configure React app for deployment to Github pages

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,49 +1,55 @@
-// import React, { useState } from 'react'
-import useForm from './Read';
+import React, { useState } from 'react'
+// import useForm from './Read';
 import './TodoApp.css';
 
 const TodoApp = () => {
-  var [toDos,setToDos] = useForm({todo:"",qty:"",des:""})
-  // var [toDo,setToDo] = useState('')
+  var [toDos,setToDos] = useState([])
+  //  useForm({todo:"",qty:"",des:""})
+  var [toDo,setToDo] = useState('')
 
   const handleSubmit =(e) =>{
     e.preventDefault();
   }
 
-  // const addTodo = () => {
-  //   setToDos ([...toDos,toDo])
-  //   console.log(toDos)
-  //   setToDo (' ')
-  // }
+  const addTodo = () => {
+    setToDos ([...toDos,toDo])
+    console.log(toDos)
+    setToDo (' ')
+  }
   return (
 
     <div className='header'>
       <h1>ToDo List</h1>
       <form className='input-sect' onSubmit={handleSubmit}>
         <input name='todo'
-        value={toDos.todo} onChange={setToDos}
-        type="tex" placeholder='Enter Todo Items'></input><br></br>
-        <input name='qty'
+        value={toDo} onChange={(event)=>setToDo(event.target.value)}
+        type="tex" placeholder='Enter Todo Items'></input>
+        {/* <input name='qty'
         value={toDos.qty} onChange={setToDos}
         type="tex" placeholder='Enter quantity'></input><br></br>
         <input name='des'
         value={toDos.des} onChange={setToDos}
-        type="tex" placeholder='Enter description'></input>
-        <button>ADD</button>  </form>
+        type="tex" placeholder='Enter description'></input> */}
+        <button onClick={addTodo}>ADD</button>
+      </form>
       
       <div className='todos'>
-        {/* { toDos.map((value)=>{
+        { toDos.map((value)=>{
 
-      return ( */}
+      return (
         <div className='todo'>
-            <p>{toDos.todo}</p><br></br>
-            <p>{toDos.qty}</p><br></br>
-            <p>{toDos.des}</p>
-        </div>
-        {/* ) */}
-          {/* }) */}
+          <p>{value}
+          {/* <button id='dlt' onClick={}>Delete</button> */}
+          </p>
 
-        {/* } */}
+            {/* // <p>{toDos.todo}</p><br></br>
+            // <p>{toDos.qty}</p><br></br>
+            // <p>{toDos.des}</p> */}
+        </div>
+        )
+          })
+
+        }
       </div>
         
      
